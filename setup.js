27488004c29e82ede26629efdd829f6c7615e61f
David Balla <szefoka@gmail.com>
2020-07-03 15:26:06 +0200

    Update setup.js

--- setup.js
+++ setup.js
@@ -111,7 +111,7 @@ if (config.machines !== undefined) {
     const machine = config.machines[i];
     machines.push(Machine({
       id: i,
-      privateIP: machine.runtimeNetworks[0].ip,
+      privateIP: "10.10.0.1", //machine.runtimeNetworks[0].ip,
       sshPort: sshPort(machine),
       role: 'master',
     }));
@@ -121,7 +121,7 @@ if (config.machines !== undefined) {
     const machine = config.machines[config.controlPlane.nodes + i];
     machines.push(Machine({
       id: i,
-      privateIP: machine.runtimeNetworks[0].ip,
+      privateIP: "10.10.0.2",  //machine.runtimeNetworks[0].ip,
       sshPort: sshPort(machine),
       role: 'worker',
     }));
